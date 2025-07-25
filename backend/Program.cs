f1e4290fd09a1f4df6e143a3f2ea34686d79d7c4
Victor <victorjuez.alu@gmail.com>
2025-07-25 07:04:46 +0200

    Fix database connection string to use configuration and default to TaskTracker.db

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TaskContext>(options =>
-    options.UseSqlite("Data Source=tasks.db"));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=TaskTracker.db"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
